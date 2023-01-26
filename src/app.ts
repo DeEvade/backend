586360c311c79ceb08c4cf173dd7b5812cde52da
DeEvade <william.redgard@gmail.com>
2023-01-26 04:11:00 +0100

    first

---
+++ src/app.ts
@@ -0,0 +1,58 @@
+import express from 'express';
+const cors = require('cors');
+const { uuidv4 } = require('uuidv4');
+const app = express();
+app.use(cors());
+const port = process.env.port || 8080;
+const http = require('http').Server(app);
+
+interface player {
+  name: string;
+  x: number;
+  y: number;
+}
+
+let players = [] as player[];
+
+const socketIO = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+  },
+});
+
+socketIO.on('connection', (socket) => {
+  console.log('user connected');
+  socket.emit('init', players);
+  let name = null;
+
+  socket.on('createPlayer', (payload) => {
+    console.log('Creating player');
+
+    name = payload.name;
+
+    socketIO.emit('createPlayer', payload);
+    players.push(payload);
+  });
+
+  socket.on('updatePlayer', (payload) => {
+    players = players.filter((player) => player.name != payload.name);
+    players.push(payload);
+
+    socketIO.emit('updatePlayer', payload);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected', name);
+    socketIO.emit('deletePlayer', { name: name, x: 0, y: 0 });
+    players = players.filter((player) => player.name != name);
+  });
+});
+
+app.get('/', (req, res) => {
+  console.log('home page accessed');
+  res.send(players);
+});
+
+http.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
