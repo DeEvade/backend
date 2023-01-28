a9cbee4abfc4d1ea3aa50c9a00cb5d0f028992e4
DeEvade <william.redgard@gmail.com>
2023-01-28 03:35:06 +0100

    WASD update

--- src/app.ts
+++ src/app.ts
@@ -7,10 +7,12 @@ const port = process.env.port || 8080;
 const http = require('http').Server(app);
 
 interface player {
+  id: string;
   name: string;
   hunter: boolean;
   x: number;
   y: number;
+  alive: boolean;
 }
 
 //Hej
@@ -26,33 +28,52 @@ const socketIO = require('socket.io')(http, {
 socketIO.on('connection', (socket) => {
   console.log('user connected');
   socket.emit('init', players);
-  let name = null;
+  let id = null;
 
   socket.on('createPlayer', (payload) => {
     console.log('Creating player');
+    let index = -1;
+    for (let i = 0; i < players.length; i++) {
+      if (players[i].id == payload.id) index = i;
+    }
 
-    name = payload.name;
-
-    socketIO.emit('createPlayer', payload);
-    players.push(payload);
+    if (index === -1) {
+      id = payload.id;
+      socketIO.emit('createPlayer', payload);
+      players.push(payload);
+    } else {
+      players[index] = payload;
+      socketIO.emit('updatePlayer', players[index]);
+    }
   });
 
   socket.on('updatePlayer', (payload) => {
-    players = players.filter((player) => player.name != payload.name);
-    players.push(payload);
+    const index = players.findIndex((player) => player.id == payload.id);
+    if (players[index]) {
+      players[index].x = payload.x;
+      players[index].y = payload.y;
 
-    socketIO.emit('updatePlayer', payload);
+      socketIO.emit('updatePlayer', players[index]);
+    }
   });
 
   socket.on('deletePlayer', (payload) => {
-    socketIO.emit('deletePlayer', { name: name, x: 0, y: 0 });
-    players = players.filter((player) => player.name != name);
+    console.log('delete player', payload.name);
+    payload.alive = false;
+    socketIO.emit('updatePlayer', payload);
+    for (let i = 0; i < players.length; i++) {
+      const player = players[i];
+      if (player.id == payload.id) {
+        players[i].alive = false;
+      }
+    }
   });
 
   socket.on('disconnect', () => {
-    console.log('user disconnected', name);
-    socketIO.emit('deletePlayer', { name: name, x: 0, y: 0 });
-    players = players.filter((player) => player.name != name);
+    console.log('user disconnected', id);
+
+    socketIO.emit('deletePlayer', { id: id, name: '', x: 0, y: 0 });
+    players = players.filter((player) => player.id != id);
   });
 });
 
